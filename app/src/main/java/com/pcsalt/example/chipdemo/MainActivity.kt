45b43e02efae42cb3ae1e0d5f7931e54f223a9a0
krrishnaaaa <krishna@pcsalt.com>
2021-05-23 12:03:36 +0530

    clearing AutoCompleteTextView after planet is selected

--- app/src/main/java/com/pcsalt/example/chipdemo/MainActivity.kt
+++ app/src/main/java/com/pcsalt/example/chipdemo/MainActivity.kt
@@ -46,6 +46,7 @@ class MainActivity : AppCompatActivity() {
             } else {
                 addPlanetChip(selectedPlanet)
             }
+            binding.tietPlanets.setText("")
         }
     }
 
